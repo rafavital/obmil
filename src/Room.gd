b6aa5c417885c6ee93cbb9df3ec7512c055bef85
Rafael Vital <rvitall.alves@gmail.com>
2021-02-26 19:31:21 -0300

    working room randomization

--- src/Room.gd
+++ src/Room.gd
@@ -1,9 +1,9 @@
+class_name Room
 extends Spatial
 
-
-
+signal exited_room (room)
 
 func _on_ExitTrigger_body_entered(body: Node) -> void:
 	var dir = sign(transform.basis.z.dot(body.transform.basis.z))
 	if dir > 0:
-		queue_free()
+		emit_signal("exited_room", self)
---
+++ src/RoomsManager.gd
@@ -0,0 +1,25 @@
+extends Spatial
+
+export (Array, PackedScene) var rooms
+var room_nodes = []
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	for	room in get_children():
+		room.connect("exited_room", self, "_on_generate_new_room")
+		
+
+
+func _on_generate_new_room (room) -> void:
+	var pos = room.transform.origin
+	room.queue_free()
+	
+	#make room truly random
+	var id = rng.randi_range(0, rooms.size() - 1)
+	
+	var r = rooms[id].instance()
+	
+	r.transform.origin = pos
+	r.connect("exited_room", self, "_on_generate_new_room")
+	
+	add_child(r)
