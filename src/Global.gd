ae40933e4b326cecc29d52f8ffe7a76ae3e902ca
Rafael Vital <rvitall.alves@gmail.com>
2021-03-01 00:48:50 -0300

    SOCORRO

--- src/Global.gd
+++ src/Global.gd
@@ -4,4 +4,8 @@ const en_dialogues_path := 'res://assets/dialogue/dialogue_en.json'
 #const en_dialogues_path := res://assets/dialogue/dialogue_en.json
 var dialogues = {}
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_select"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
 
