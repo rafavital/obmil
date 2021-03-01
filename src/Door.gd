22afd9b7a003c496c1127c6b207d6f4b9ffe2b44
Rafael Vital <rvitall.alves@gmail.com>
2021-02-28 21:00:16 -0300

    room improvements

--- src/Door.gd
+++ src/Door.gd
@@ -9,4 +9,14 @@ func _on_ExitTrigger_body_entered(body: Node) -> void:
 
 
 func _on_ExitTrigger_area_entered(area: Area) -> void:
+	pass
+	#emit_signal("exited_room", self, area)
+
+
+func _on_ExitTrigger_body_exited(body: Node) -> void:
+	pass
+	#emit_signal("exited_room", self, body)
+
+
+func _on_ExitTrigger_area_exited(area: Area) -> void:
 	emit_signal("exited_room", self, area)
