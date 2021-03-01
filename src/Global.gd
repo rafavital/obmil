213170ded51299e092a88dcc0d98e7e5dc36c102
Rafael Vital <rvitall.alves@gmail.com>
2021-03-01 00:50:10 -0300

    FULLSCREEN

--- src/Global.gd
+++ src/Global.gd
@@ -5,7 +5,7 @@ const en_dialogues_path := 'res://assets/dialogue/dialogue_en.json'
 var dialogues = {}
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_select"):
+	if event.is_action_pressed("fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
 
 
